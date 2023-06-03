984f738e0c3f719e46c63f10c7185a952a53b66b
anjali <anjalirao115@gmail.com>
2023-06-03 14:39:08 +0100

    Generates final data file for visualization

---
+++ gen_data_for_viz.py
@@ -0,0 +1,24 @@
+#%%
+import pandas as pd
+
+# reading in the data on vegan friendly restaurants in different cities
+df_1 = pd.read_csv("city_vegan_restaurant_data.csv")
+
+# reading in the data on population in different cities
+df_2 = pd.read_csv("city_population_data.csv")
+
+# reading into variables
+city                = df_1['city']
+vegan_restaurant    = df_1['vegan_restaurant']
+population_2023     = df_2['population_2023']
+lat                 = df_2['lat']
+long                = df_2['long']
+vegan_restaurant_per_capita = vegan_restaurant/population_2023
+
+# defining a combined dataframe
+df = pd.DataFrame({ 'city': city, 'vegan_restaurant': vegan_restaurant,  'population_2023': population_2023,
+                   'vegan_restaurant_per_capita': vegan_restaurant_per_capita, 'lat': lat, 'long': long })
+
+# writing the combined dataframe to csv file
+df.to_csv('output.csv', index=False)
+# %%
